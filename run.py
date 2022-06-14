56dba673ce4f151aaf63f9d62cc45f387c9f616f
abrech <adrian.brechtken@gmail.com>
2022-06-14 14:11:00 +0200

    host

--- run.py
+++ run.py
@@ -73,7 +73,7 @@ def home():
 atexit.register(lambda: sched.shutdown())
 
 if __name__ == "__main__":
-    app.run(port=6400)
+    app.run(host='0.0.0.0', port=6400)
 """
 # checks pending schedules
 while True:
